a7e2434d0bce962cb36e5274369c5cc14afe9522
aditya3438 <aditya3438@gmail.com>
2019-04-16 17:39:45 +0530

    font changes in navbar

--- assess02f-focus-phase/Frontend/submissions/cubetimer/cubetimer/src/components/layout/Navbar.js
+++ assess02f-focus-phase/Frontend/submissions/cubetimer/cubetimer/src/components/layout/Navbar.js
@@ -11,14 +11,14 @@ class Navbar extends Component{
                 <div className ="nav-wrapper">
                     <nav className = "z-depth-0">
                         <div className = "nav-wrapper-white">
-                            <Link to ="/" style={{fontFamily:"monospace"}} className="col s5 brand-logo center black-text">
+                            <Link to ="/" style={{fontFamily:"monospace"}} className="col s3 brand-logo center black-text">
                                 <i className ="material-icons">timer</i>
                                 <b>CuTi</b>
                                 {/* <i className ="material-icons">offline_bolt</i> */}
                             </Link>
                             <ul id="nav-mobile" class="right hide-on-med-and-down">
                             <li>
-                                <Link to ="/about" style={{fontFamily:"monospace"}} className="col s5">
+                                <Link to ="/about" style={{fontFamily:"monospace"}} className="col s5 flow-text">
                                     About
                                 </Link>
                             </li>
